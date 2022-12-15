be3201d5695de516a8ba0f7bfaac66b0019543d9
bekhruz <yoshlikmedia@gmail.com>
2022-12-15 13:59:33 +0300

    🔗 | Barcha handlerlar birlashtirildi, ortiqcha kataloglardan voz kechildi.

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,4 +1,5 @@
-from . import errors
-from . import channels
-from . import groups
-from . import users
+# Bu chiqishi mumkin bo'lgan errorlar uchun ( O'chirish tavsiya etilmaydi )
+from . import error_handler
+
+# Barcha handlerlarni shu yerdan boshlab qo'shib ketishni tavsiya qilamiz.
+from . import test
